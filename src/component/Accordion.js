3dcf5cb22d744e37785bfccb1f552bd6992dbe40
zeeroiq <shrikantvishwakarma@outlook.com>
2021-04-30 00:19:47 +0530

    added Accordion, closes #1

---
+++ src/component/Accordion.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+const Accordion = ({ languages }) => {
+
+    const [activeLanguage, setActiveLanguage] = useState(null);
+
+    const onTitleClick = currentLanguage => {
+        setActiveLanguage(currentLanguage)
+    }
+
+    const renderItems = languages.map((language, index) => {
+
+        const activeLang = index === activeLanguage ? 'active' : '';
+
+        return (
+            <React.Fragment key={language.title}>
+                <div className={`title ${ activeLang }`} onClick={() => onTitleClick(index)}>
+                    <i className={`dropdown icon`} />
+                    {language.title}
+                </div>
+                <div className={`content ${ activeLang }`}>
+                    <p>{language.description}</p>
+                </div>
+            </React.Fragment>
+        )
+    })
+
+    return (
+        <div className={`ui styled accordion`}>
+            {renderItems}
+        </div>
+    );
+};
+
+export default Accordion;
--- src/component/App.js
+++ src/component/App.js
@@ -1,9 +1,46 @@
 import React from "react";
+import Accordion from "./Accordion";
+
+
+const programmingLanguages = [
+    {
+        title: 'Java',
+        description: 'A backend programming language'
+    },
+    {
+        title: 'JavaScript',
+        description: 'A language for the web'
+    },
+    {
+        title: 'C',
+        description: 'A powerful general-purpose programming language'
+    },
+    {
+        title: 'C++',
+        description: 'A powerful general-purpose programming language, superset of C'
+    },
+    {
+        title: 'Python',
+        description: 'A multi-paradigm programming language'
+    },
+    {
+        title: 'Go',
+        description: 'A procedural programming language'
+    },
+    {
+        title: 'Kotlin',
+        description: 'Gradle is introducing Kotlin as a language for writing build scripts'
+    },
+    {
+        title: 'Swift',
+        description: 'Language for macOS, iOS, watchOS, tvOS and beyond'
+    }
+]
 
 function App() {
   return (
     <div>
-      <h1>Widget</h1>
+      <Accordion languages={programmingLanguages}/>
     </div>
   );
 }
