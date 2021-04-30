3e90e1eda7f3e033a2b795bbf67250d0833f8a94
zeeroiq <shrikantvishwakarma@outlook.com>
2021-05-01 01:36:09 +0530

    completed Search component implementation. closes #2

--- src/component/Search.js
+++ src/component/Search.js
@@ -3,39 +3,50 @@ import axios from 'axios';
 
 const Search = () => {
     const [term, setTerm] = useState('programming');
+    const [debTerm, setDebTerm] = useState(term);
     const [results, setResults] = useState([]);
-    console.log(results);
+    // console.log(results);
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebTerm(term)
+        }, 1000);
+
+        return () => {
+            clearTimeout(timerId);
+        }
+    }, [term])
+
     useEffect(() => {
 
-        const search = async () => {
+        (async () => {
             const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
                 params: {
                     action: 'query',
                     list: 'search',
                     origin: '*',
                     format: 'json',
-                    srsearch: term
+                    srsearch: debTerm
                 }
             });
             setResults(data.query.search);
-        };
+        })();
+        // if(term && !results.length) {
+        //     search();
+        // } else {
+        //     const timeOutId = setTimeout(() => {
+        //         if (term) {
+        //             search()
+        //         }
+        //     }, 1000);
+        //
+        //
+        //     return () => {
+        //         clearTimeout(timeOutId);
+        //     };
+        // }
 
-        if(term && !results.length) {
-            search();
-        } else {
-            const timeOutId = setTimeout(() => {
-                if (term) {
-                    search()
-                }
-            }, 1000);
-
-
-            return () => {
-                clearTimeout(timeOutId);
-            };
-        }
-
-    }, [term])
+    }, [debTerm])
 
     const renderResults = results.map(result => {
         return (
