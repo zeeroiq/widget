f450433208c8591691113e512dcbb9b523c722b3
zeeroiq <shrikantvishwakarma@outlook.com>
2021-04-30 11:34:09 +0530

    fixed html code formation, closes #3

--- src/component/Search.js
+++ src/component/Search.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios';
 
 const Search = () => {
-    const [term, setTerm] = useState('');
+    const [term, setTerm] = useState('programming');
     const [results, setResults] = useState([]);
     console.log(results);
     useEffect(() => {
@@ -26,11 +26,19 @@ const Search = () => {
     const renderResults = results.map(result => {
         return (
             <div className={`item`} key={result.pageid}>
+                <div className={`right floated content`}>
+                    <a className={`ui button`}
+                       href={`https://en.wikipedia.org?curid=${result.pageid}`}
+                    >
+                        Go
+                    </a>
+
+                </div>
                 <div className={`content`}>
                     <div className={`header`}>
                         {result.title}
                     </div>
-                    {result.snippet}
+                    <span dangerouslySetInnerHTML={{ __html: result.snippet }} />
                 </div>
             </div>
         )
