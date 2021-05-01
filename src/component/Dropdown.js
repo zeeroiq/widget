f4a41c3fa08442f4607360e684b9450960593b9b
zeeroiq <shrikantvishwakarma@outlook.com>
2021-05-02 00:55:29 +0530

    manual event listener to close the dropdown, clicking anywhere on window. closes #6

--- src/component/Dropdown.js
+++ src/component/Dropdown.js
@@ -1,9 +1,24 @@
-import React, {useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 
 
 const Dropdown = ({ options, select, onSelectChange }) => {
 
     const [open, setOpen] = useState(false);
+    const ref = useRef();
+
+    useEffect(() => {
+
+        document.body.addEventListener('click', (event) => {
+            // if (ref.current.contains(event.target)) {
+            //     return;
+            // }
+            if (ref.current && ref.current.contains(event.target)) {
+                return;
+            }
+            setOpen(false);
+        }, {capture: true});
+
+    }, []);
 
     const renderedOption = options.map(option => {
         if (option.value === select.value) {
@@ -20,7 +35,7 @@ const Dropdown = ({ options, select, onSelectChange }) => {
     });
 
     return (
-        <div className={`ui form`}>
+        <div ref={ ref } className={`ui form`}>
             <div className={`field`}>
                 <label className={`label`}>Select a Color</label>
                 <div onClick={ () => setOpen(!open) } className={`ui selection dropdown ${ open ? 'visible active' : ''}`}>
